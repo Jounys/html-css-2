33ac987f0d03a9f188b102b0807affa1d66c37c0
João Paulo <103288654+Jounys@users.noreply.github.com>
2024-04-24 10:42:08 -0300

    Pratica de formularios

---
+++ exercicios/ex025/cadastro.js
@@ -0,0 +1,8 @@
+Dados não estão aqui.
+
+// function enviar(){
+//     let nome = document.querySelector('#inome').value 
+//     let sobrenome = document.querySelector('#isobrenome').value
+
+//     document.querySelector('#res').innerHTML = `Olá Sr. ${nome} ${sobrenome}`
+// }
